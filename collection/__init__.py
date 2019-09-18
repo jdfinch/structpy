b03dabeb1570312e145f2a18a83fa6d89c99940a
James Finch <jdfinch@emory.edu>
2019-09-18 02:14:31 -0400

    added bidictionary

--- collection/__init__.py
+++ collection/__init__.py
@@ -1,3 +1,4 @@
 from structpy.collection.queue import Queue
 from structpy.collection.priority_queue import PriorityQueue
 from structpy.collection.stack import Stack
+from structpy.collection.bidictionary import Bidictionary
