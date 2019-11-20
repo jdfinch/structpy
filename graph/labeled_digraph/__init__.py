32ae89b3e6d404c7297ccbdbf7c298664a4ed84b
James Finch <jdfinch@emory.edu>
2019-11-20 16:23:28 -0500

    changed __init__

--- graph/labeled_digraph/__init__.py
+++ graph/labeled_digraph/__init__.py
@@ -1,2 +1,4 @@
 from structpy.graph.labeled_digraph.labeled_digraph import LabeledDigraph
 from structpy.graph.labeled_digraph.map_digraph import MapDigraph
+from structpy.graph.labeled_digraph.map_data_digraph import MapDataDigraph
+
