e07050015909e0d5d47b19d2d226ec5eca826188
James Finch <jdfinch@emory.edu>
2019-12-17 17:04:54 -0500

    graph traversal init updated

--- graph/traversal/__init__.py
+++ graph/traversal/__init__.py
@@ -1,6 +1,6 @@
 
 from structpy.graph.traversal.traversal import Traversal
 from structpy.graph.traversal.rings import rings
-import structpy.graph.traversal.preset as traversals
+from structpy.graph.traversal import preset as traversals
 
 
