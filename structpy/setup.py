9f8440cfaab995eb49d68822252afe062827488e
James Finch <jdfinch@emory.edu>
2019-12-21 01:42:37 -0500

    changed directory structure in line with pip package

---
+++ structpy/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+setuptools.setup(
+     name='structpy',
+     version='0.1',
+     scripts=[],
+     author="James Finch",
+     author_email="jdfinch@emory.edu",
+     description="Graph algorithms and data structures",
+     long_description=long_description,
+   long_description_content_type="text/markdown",
+     url="https://github.com/jdfinch/structpy",
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+ )
+
